23bbb563b074e77555d2cf89fa10406dbc424a5c
zilola <egamberganovazilola1030@gmail.com>
2026-09-23 13:41:47 +0500

    Update views without login required

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from . import forms
 from . import services
-from products.filters import Product_Filter
+from products.filters import ProductFilter
 
 def home_page(request):
     products = Product.objects.all()
@@ -138,7 +138,7 @@ def category_delete(request, pk):
 @login_required_decarator
 def product_list(request):
     products = Product.objects.all()
-    product_filter = Product_Filter(request.GET, queryset=products)
+    product_filter = ProductFilter(request.GET, queryset=products)
     return render(request, "dashboard/product_list.html", {'products': products, 'filter': product_filter})
 
 @login_required_decarator
